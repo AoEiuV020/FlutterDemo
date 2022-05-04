428a7974ab9c66bd793d1f6ca5f37c42237cc6e3
AoEiuV020 <aoeiuv020@gmail.com>
2022-05-04 20:05:28 +0800

    改成直接获取文本并打印

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,16 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum(http.Client client) async {
+Future<String> fetchContent(http.Client client) async {
   final response = await client
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+      .get(Uri.parse('https://example.com/'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
+    return response.body;
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({required this.userId, required this.id, required this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
+    throw Exception('Failed to load content');
   }
 }
 
@@ -45,12 +24,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late final Future<Album> futureAlbum;
+  late final Future<String> futureContent;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum(http.Client());
+    futureContent = fetchContent(http.Client());
   }
 
   @override
@@ -65,11 +44,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
+          child: FutureBuilder<String>(
+            future: futureContent,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data!.title);
+                return Text(snapshot.requireData);
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
