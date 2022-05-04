9bf435257096ae1b90829534afd052b0f928c900
AoEiuV020 <aoeiuv020@gmail.com>
2022-05-05 00:29:42 +0800

    添加注释--allow-running-insecure-content

--- lib/mixed.dart
+++ lib/mixed.dart
@@ -3,6 +3,7 @@ import 'package:html/parser.dart' show parse;
 
 /// web端https网站请求http会有mixed-content问题，浏览器规范限制，不是代码能解决的，
 /// chrome可以在网站设置(Site settings)中允许不安全内容(Insecure content)就可以了，
+/// 或者添加参数--allow-running-insecure-content也能解除这个限制，
 /// 设置无效的话可以尝试清除网站数据(Clear data)再刷新，
 /// 恶心了，清除数据也只生效一次，
 /// 一旦运行了一次，安装上了flutter service worker,
