84d35acbb2825bb195ff26548cd22f94be706d5e
AoEiuV020 <aoeiuv020@gmail.com>
2022-05-06 00:16:06 +0800

    添加cors拓展注释

--- lib/cors.dart
+++ lib/cors.dart
@@ -4,6 +4,8 @@ import 'package:html/parser.dart' show parse;
 /// web端会有跨域（CORS）问题，浏览器规范限制，不是代码能解决的，
 /// chrome可以添加启动参数（--disable-web-security）关闭这个限制，
 /// https://stackoverflow.com/a/66879350
+/// 也可以通过安装拓展解决，
+/// https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf/related?hl=en-US
 /// 安卓chrome也可以通过adb权限设置这个参数，
 /// https://stackoverflow.com/a/52948221
 Future<String> fetchContent(http.Client client) async {
