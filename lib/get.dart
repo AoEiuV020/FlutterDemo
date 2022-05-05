a4d03d59bec9dccd5248411d7905f4f50115b6e6
AoEiuV020 <aoeiuv020@gmail.com>
2022-05-06 00:08:11 +0800

    打印headers

--- lib/get.dart
+++ lib/get.dart
@@ -5,8 +5,23 @@ Future<String> fetchUrl(http.Client client, String url) async {
       .get(Uri.parse(url));
 
   if (response.statusCode == 200) {
-    return response.body;
+    var sb = StringBuffer();
+    if (response.request != null) {
+      sb.writeAll(map(response.request!.headers));
+    }
+    // web端无法得到"set-cookie",
+    sb.writeAll(map(response.headers));
+    sb.write(response.body);
+    return sb.toString();
   } else {
     throw Exception('Failed to load content');
   }
+}
+
+List<String> map(Map<String, String> headers)  {
+  var ret = headers.entries
+      .map((e) => "<-- ${e.key}: ${e.value}\n")
+      .toList();
+  ret.sort();
+  return ret;
 }
