c4b9f78169d19ca7d1f7f6679a4a22355ce880be
AoEiuV020 <aoeiuv020@gmail.com>
2023-04-27 23:04:08 +0800

    8

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,12 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void remove(WordPair pair) {
+    if (favorites.remove(pair)) {
+      notifyListeners();
+    }
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -58,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -142,6 +148,38 @@ class GeneratorPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: IconButton(
+                onPressed: () {
+                  appState.remove(pair);
+                },
+                icon: Icon(Icons.favorite)),
+            title: Text(pair.asCamelCase),
+          ),
+      ],
     );
   }
 }
