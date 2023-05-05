9f53cb4ac306fdd083da4f8687bbbe118e967631
AoEiuV020 <aoeiuv020@gmail.com>
2023-05-06 00:33:41 +0800

    标题添加自己id

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -19,7 +19,7 @@ class SampleItemListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Items'),
+        title: const Text('Sample Items AoEiuV020'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
