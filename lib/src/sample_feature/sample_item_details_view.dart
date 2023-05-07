fca189c14fc5b15e24e1eaa6fbef289ca1236368
AoEiuV020 <aoeiuv020@gmail.com>
2023-05-08 00:29:57 +0800

    改用Stream
    有些不明白，
    一方面好像并没有实现异步加载，transform转换过程会卡主线程，
    一方面snapshot只会出现当前加载中最后一个数据，不全，

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
@@ -14,33 +15,75 @@ class SampleItemDetailsView extends StatelessWidget {
   Future<String> getFuture() async {
     try {
       return currentFile.readAsStringSync();
-    } on FileSystemException catch(_) {
+    } on FileSystemException catch (_) {
       return String.fromCharCodes(currentFile.readAsBytesSync());
     }
   }
 
+  Stream<String> getStream() {
+    log("getStream");
+    return currentFile.openRead().transform(const StringConverter());
+  }
+
   @override
   Widget build(BuildContext context) {
+    var sb = StringBuffer();
     return Scaffold(
       appBar: AppBar(
         title: Text(currentFile.path),
       ),
       body: Center(
-        child: FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            if (!snapshot.hasData) {
-              return Text("loading ${currentFile.path}");
-            }
-            return SingleChildScrollView(
-              child: Text(snapshot.requireData),
-            );
-          }
-        ),
+        child: StreamBuilder(
+            stream: getStream(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              if (!snapshot.hasData) {
+                return Text("loading ${currentFile.path}");
+              }
+              log("snapshot: ${snapshot.requireData.length}");
+              sb.write(snapshot.requireData);
+              log("sb: ${sb.length}");
+              return SingleChildScrollView(
+                child: Text(sb.toString()),
+              );
+            }),
       ),
     );
+  }
+}
+
+class StringConverter extends Converter<List<int>, String> {
+  const StringConverter();
+
+  @override
+  String convert(List<int> input) {
+    log("convert");
+    return String.fromCharCodes(input);
+  }
+
+  @override
+  Sink<List<int>> startChunkedConversion(Sink<String> sink) {
+    log("startChunkedConversion");
+    return StringSink(sink);
+  }
+}
+
+class StringSink extends Sink<List<int>> {
+  StringSink(this.sink);
+
+  final Sink<String> sink;
+
+  @override
+  void add(List<int> data) {
+    log("add: ${data.length}");
+    sink.add(String.fromCharCodes(data));
+  }
+
+  @override
+  void close() {
+    log("close");
+    sink.close();
   }
 }
