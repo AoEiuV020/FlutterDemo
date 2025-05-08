c53f454335cca021106c0c84f8f31630ab50a317
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-09 01:49:14 +0800

    调整dart调用js方式
    改用新库js_interop，删除没用的东西，

--- packages/native_add/lib/go_web.dart
+++ packages/native_add/lib/go_web.dart
@@ -1,22 +1,12 @@
-// ignore_for_file: deprecated_member_use, avoid_web_libraries_in_flutter, non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names
 
-import 'dart:js';
-import 'dart:js_interop' as js_interop;
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 @JS()
 @staticInterop
 class JSWindow {}
 
 extension JSWindowExtension on JSWindow {
-  external String Function(String input) get evalCommand;
   external int sum(int a, int b);
-  external js_interop.JSPromise<js_interop.JSNumber> sum_long_running(int a, int b);
-}
-
-@JS()
-class Go {
-  external Go();
-  external dynamic run(instance);
-  external JsObject importObject();
+  external JSPromise<JSNumber> sum_long_running(int a, int b);
 }
